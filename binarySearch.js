b13d043a5c0854163ea8bfbc5e920c2f27d56923
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2024-04-15 17:26:47 +0300

    create function find element cycle

--- binarySearch.js
+++ binarySearch.js
@@ -1,14 +1,33 @@
-console.log(createArr(50));
+const arr = createArr(-1, 5);
+console.log(findElementCycle(arr, 5));
 
-function createArr(quantityElements) {
-  const arr = [...new Array(quantityElements)].map((el) =>
-    Math.floor(Math.random() * (100 - -100) + -100)
-  );
+function createArr(firstNumber, lastNumber) {
+  const arr = [];
+  let firstElement = firstNumber;
+  for (let i = 0; i < Math.abs(firstNumber) + (lastNumber + 1); i++) {
+    arr.push(firstElement++);
+  }
   return arr;
 }
 
-function findElementCycle(arr) {
-  let left = -1;
+function findElementCycle(arr, el) {
+  let left = 0;
   let right = arr.length - 1;
-  
+  let position = -1;
+  let found = false;
+  let middle;
+
+  while (found === false && left <= right) {
+    middle = Math.floor((left + right) / 2);
+
+    if (arr[middle] === el) {
+      found = true;
+      position = middle;
+    } else if (arr[middle] > el) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return position;
 }
